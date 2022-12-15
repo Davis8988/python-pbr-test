b153bdf6404a1f2e88660920c42de1be019498ac
David Yair <Davismarsel@gmail.com>
2022-12-15 15:27:22 +0200

    Added script: pip\_complete_upgrade_flow.bat

---
+++ pip/_complete_upgrade_flow.bat
@@ -0,0 +1,16 @@
+@echo off
+
+echo.
+
+title Complete upgrade flow
+
+call "%~dp0compile_pkg.bat" || echo Error - Failure from: script: compile_pkg.bat && timeout /t 10 && exit 1
+call "%~dp0install_upgrade_pkg.bat" || echo Error - Failure from: script: install_upgrade_pkg.bat && timeout /t 10 && exit 1
+call "%~dp0push_pkg.bat" || echo Error - Failure from: script: push_pkg.bat && timeout /t 10 && exit 1
+
+echo.
+echo Finished
+echo.
+
+pause
+
